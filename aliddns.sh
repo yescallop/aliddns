34a1a53e2a319dec99a38d3f4479a62908f9d1f0
Scallop Ye <yescallop@gmail.com>
2023-11-03 01:46:07 +0800

    Fix indentation

--- aliddns.sh
+++ aliddns.sh
@@ -8,8 +8,8 @@ urlencode() {
   for (( i = 0; i < length; i++ )); do
     local c="${1:i:1}"
     case $c in
-        [a-zA-Z0-9.~_-]) printf "$c" ;;
-        *) printf '%%%02X' "'$c" ;;
+      [a-zA-Z0-9.~_-]) printf "$c" ;;
+      *) printf '%%%02X' "'$c" ;;
     esac
   done
 }
